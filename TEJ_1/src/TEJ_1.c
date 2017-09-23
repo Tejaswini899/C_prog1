bbb1e151d1ef86a5268219e5c62d28bca67e8ee7
BALAJINV <BALAJINV@DESKTOP-UTO41MT>
2017-09-23 19:35:52 +0530

    just commit

---
+++ TEJ_1/src/TEJ_1.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : TEJ_1.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void) {
+	printf("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+}
