b5b1ee160113b3c2d18a05fa914f2f5d11c57ea2
BALAJINV <BALAJINV@DESKTOP-UTO41MT>
2017-09-23 20:07:29 +0530

    nothingggg

--- TEJ_1/src/TEJ_1.c
+++ TEJ_1/src/TEJ_1.c
@@ -11,7 +11,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main(void) {
-	printf("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+int main(void) {
 
-}
+	/*int m, n, i, j, k, a[10][10], b[10][10], c[10][10];*/
+
+	printf("enter m and n");
+	/*scanf("%d%d", &m, &n);
+	printf("enter values for matrix 1");
+	for (i = 0; i < m; i++) {
+		for (j = 0; i < n; j++)
+			scanf("%d", &a[i][j]);
+	}
+
+	printf("enter values for matrix 2");
+	for (i = 0; i < m; i++) {
+		for (j = 0; i < n; j++)
+			scanf("%d", &b[i][j]);
+	}
+
+	while (m == n) {
+		for (i = 0; i < m; i++) {
+			for (j = 0; j < n; j++) {
+				for (k = 0; k < n; k++)
+					c[i][j] = c[i][j] + (a[i][k] * b[k][j]);
+			}
+		}
+		for (i = 0; i < m; i++)
+		   {
+			for (j = 0; j < n; j++){
+				printf("%d\t", c[i][j]);
+		    }
+		printf("\n");
+	}
+}*/
+	return 0;
+	}
+
+
